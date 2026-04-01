ae0687afd53d8b2f2dfab70a95db49809425110c
welloul <22amps@gmail.com>
2026-04-01 06:10:21 +0200

    Final hyperliquid sdk trait fix

--- src/eal/hyperliquid_exec.rs
+++ src/eal/hyperliquid_exec.rs
@@ -14,6 +14,7 @@ use crate::eal::{
     Position, Symbol, VenueId,
 };
 
+use hyperliquid::{Exchange, Hyperliquid};
 use hyperliquid::types::{
     Chain,
     exchange::request::{OrderRequest as HLOrderRequest, OrderType as HLOrderType, Limit, Tif},
